b28ace147751d51b9fc0c3e3bbe36af56f02389b
Omer abd almajeed <omeroooor@gmail.com>
2020-07-04 21:13:37 +0200

    Add product public categories

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Hilia Store',
+    'summary': "Customizing Web store for Hilia Store",
+    'version': '1.0',
+    'depends': ['website_sale'],
+    'data': [  
+        'data/product_public_categories.xml',
+    ], 
+    'installable': True,
+    'application': True,
+}
